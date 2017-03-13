8bfcdcabb88afa44839cb5309314692e199309e8
a337928 <swongtao@163.com>
2017-03-13 20:09:18 +0800

    声明注解

---
+++ src/main/java/com/arthur/annotation/query.java
@@ -0,0 +1,16 @@
+package com.arthur.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by wangtao on 17/3/13.
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface query {
+    public String value();
+
+}
